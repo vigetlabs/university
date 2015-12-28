9ada1524b3799a14fcef645bd6bde30feffa9ccb
Eli Fatsi <eli.fatsi@viget.com>
2015-12-28 16:13:03 -0500

    Using inject-then to catch test failures in hapi responses

--- test/indexTest.js
+++ test/indexTest.js
@@ -4,13 +4,15 @@ var routes = require("../lib/routes")
 
 var server = new Hapi.Server()
 
-server.connection({ host: 'test' });
-server.route(routes);
-
+server.connection({ host: 'test' })
+server.route(routes)
+server.register(require('inject-then'), function (err) {
+  if (err) throw err
+})
 it('should GET the version', function() {
   var request = { url: '/version', method: 'GET' }
 
-  server.inject(request, function (res) {
+  server.injectThen(request).then(function (res) {
     assert.equal(res.statusCode, 200)
     assert.equal(res.payload, JSON.stringify({"version": "0.0.1"}))
   })
