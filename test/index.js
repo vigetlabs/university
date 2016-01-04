df99bb0a51169a211fd8d77d22fb650b29ea414b
Doug Avery <averyquery@gmail.com>
2016-01-04 09:40:17 -0700

    Fixing bad port in test

--- test/index.js
+++ test/index.js
@@ -6,7 +6,7 @@ startServer(8001)
 
 describe('/version', () => {
   it('returns the current version', (done) => {
-    superagent.get('http://localhost:8000/version')
+    superagent.get('http://localhost:8001/version')
               .end((err, res) => {
                 assert.deepEqual({ version: '0.0.1' }, JSON.parse(res.text))
                 done()
