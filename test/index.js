db8d991a2cc1a27bd489b46e6f4a8371e61fd58e
Doug Avery <averyquery@gmail.com>
2016-01-04 10:51:16 -0700

    Removing superagent, using server.inject to test

--- test/index.js
+++ test/index.js
@@ -1,15 +1,15 @@
 import assert      from 'assert'
-import superagent  from 'superagent'
 import startServer from '../lib/server'
 
-startServer(8001)
+const server = startServer()
 
 describe('/version', () => {
   it('returns the current version', (done) => {
-    superagent.get('http://localhost:8001/version')
-              .end((err, res) => {
-                assert.deepEqual({ version: '0.0.1' }, JSON.parse(res.text))
-                done()
-              })
+    server.inject({
+      url: '/version'
+    }, (res) => {
+      assert.deepEqual({ version: '0.0.1' }, JSON.parse(res.payload))
+      done()
+    })
   })
 })
